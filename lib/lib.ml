717f0024439581a9e78a420fc4d8222d8fa1c98d
epatrizio <epatrizio@mpns.fr>
2023-03-30 17:04:11 +0200

    vscodebug

--- lib/lib.ml
+++ lib/lib.ml
@@ -6,9 +6,3 @@ let hello name lower =
   "Hello, "
   ^ (if lower then String.lowercase_ascii name else String.uppercase_ascii name)
   ^ "!"
-
-let rec token buf =
-  match%sedlex buf with
-  | "hello" -> token buf
-  | Plus "hello" -> token buf
-  | _ -> token buf
